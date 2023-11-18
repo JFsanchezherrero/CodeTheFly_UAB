a32826d74f46a1d42be4b8434b62e1466d0d7fca
Jose Francisco Sanchez-Herrero <jfbioinformatics@gmail.com>
2023-11-18 18:25:04 +0100

    Create SRA_selector.py

---
+++ SRA_selector.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+##
+print("\n###################################")
+print ("## Scrip to randomly select SRAids")
+print("###################################")
+
+print("\n + Read SRAids file...")
+SRA_file = "/data/codefly/scratch2/fly/SRAids.txt"
+print("")
+## get contents into a list
+with open(SRA_file, "r") as reader:
+    SRA_list = reader.readlines()
+
+print("+ List of available SRAs contains: ")
+print("  " + str(len(SRA_list)) + " entries")
+
+print("\n+ Randome choice...\n")
+import random
+rnd_entry = random.choice(SRA_list).replace("\n", "")
+
+print("#----------------------------#")
+print("Your SRA id assigned is: " + rnd_entry)
+print("#----------------------------#")
+print("\n")
